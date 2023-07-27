81085e2d12daa161ab82a792cf3e409ba2feb224
Vita Fomin <vita.bukhalo@gmail.com>
2023-07-26 21:23:28 -0400

    completed typeDef file

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,4 +1,32 @@
 const { gql } = require('apollo-server-express');
 
+const typeDefs = gql`
+type User {
+    _id: ID!
+    username: String!
+    email: String!
+    savedPasswords: [Password]
+}
+type Password {
+    _id: ID!
+    loginTo: String!
+    savedUsername: String!
+    savedPassword: String!
+}
+type Auth {
+    token: ID!
+    user: User
+}
+type Query {
+    user(userId: ID!): User
+    me: User
+    passwords: [Password]
+}
+type Mutation {
+    createUser(username: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+    savePassword(loginTo: String!, savedUsername: String!, savedPassword: String!): User
+    removePassword(passwordId: String!): User
+}`
 
 module.exports = typeDefs;
