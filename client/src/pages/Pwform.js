4831f19c8409b38161f1c25cedf5753663edd858
pg1219 <peter.gronseth@gmail.com>
2023-08-03 21:24:22 -0400

    fixed brackets

--- client/src/pages/Pwform.js
+++ client/src/pages/Pwform.js
@@ -1,97 +1,88 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import { useMutation } from "@apollo/client";
 import { ADD_PASSWORD } from "../utils/mutations";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 import Auth from "../utils/auth";
 
 const CreatePass = () => {
-    const [loginTo, setLoginTo] = useState("");
-    const [savedUsername, setSavedUsername] = useState("");
-    const [savedPassword, setSavedPassword] = useState("");
-    
-    const [addPass, { loading, error }] = useMutation(ADD_PASSWORD);
+  const [loginTo, setLoginTo] = useState("");
+  const [savedUsername, setSavedUsername] = useState("");
+  const [savedPassword, setSavedPassword] = useState("");
 
-    const handleFormSubmit = async (event) => {
-        event.preventDefault()
+  const [addPass, { loading, error }] = useMutation(ADD_PASSWORD);
 
-        try{
-            const { data } = await addPass({
-              variables:  { loginTo, savedUsername, savedPassword }
-            })
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
 
-            setSavedPassword("")
-            setSavedUsername("")
-            setLoginTo("")
-            console.log(data)
-        } catch (err) {
-            console.error(err);
-        }
-    };
+    try {
+      const { data } = await addPass({
+        variables: { loginTo, savedUsername, savedPassword },
+      });
 
-    return (
-
-      <div>
-        {Auth.loggedIn() ? (
-          <>
-            <h4 className="text-center custom-h4">Add the credentials you want to lock in</h4>
-
-
-
-
-            <form
-  className="flex-column align-center"
-  onSubmit={handleFormSubmit}
->
-<div className="col-12 col-lg-10 mx-auto mt-4">
-  <input
-    placeholder="Add a website..."
-    value={loginTo}
-    className="form-input w-100 mb-3"
-    onChange={(event) => setLoginTo(event.target.value)}
-  />
-  <input
-    placeholder="Add a username..."
-    value={savedUsername}
-    className="form-input w-100 mb-3"
-    onChange={(event) => setSavedUsername(event.target.value)}
-  />
-  <input
-    placeholder="Add a password..."
-    value={savedPassword}
-    className="form-input w-100 mb-3"
-    onChange={(event) => setSavedPassword(event.target.value)}
-  />
-</div>
-
-    
-
-              <div className="col-12 col-lg-9">
-
-                <button className="btn btn-info btn-block py-3" type="submit">
-                  Add Credentials
-                </button>
-              </div>
-              
-              {error && (
-                <div className="col-12 my-3 bg-danger text-white p-3">
-                  {error.message}
-                </div>
-              )}
-            </form>
-          </>
-        ) : (
-          <p>
-
-            Please login to add credentials{' '}
-
-            <Link to="/login">login</Link> or <Link to="/signup">signup</Link>
-          </p>
-        )}
-      </div>
-
-    );
+      setSavedPassword("");
+      setSavedUsername("");
+      setLoginTo("");
+      console.log(data);
+    } catch (err) {
+      console.error(err);
+    }
   };
- 
+
+  return (
+    <div>
+      {Auth.loggedIn() ? (
+        <>
+          <h4 className="text-center custom-h4">
+            Add the credentials you want to lock in
+          </h4>
+
+          <form
+            className="flex-column align-center"
+            onSubmit={handleFormSubmit}
+          >
+            <div className="col-12 col-lg-10 mx-auto mt-4">
+              <input
+                placeholder="Add a website..."
+                value={loginTo}
+                className="form-input w-100 mb-3"
+                onChange={(event) => setLoginTo(event.target.value)}
+              />
+              <input
+                placeholder="Add a username..."
+                value={savedUsername}
+                className="form-input w-100 mb-3"
+                onChange={(event) => setSavedUsername(event.target.value)}
+              />
+              <input
+                placeholder="Add a password..."
+                value={savedPassword}
+                className="form-input w-100 mb-3"
+                onChange={(event) => setSavedPassword(event.target.value)}
+              />
+            </div>
+
+            <div className="col-12 col-lg-9">
+              <button className="btn btn-info btn-block py-3" type="submit">
+                Add Credentials
+              </button>
+            </div>
+
+            {error && (
+              <div className="col-12 my-3 bg-danger text-white p-3">
+                {error.message}
+              </div>
+            )}
+          </form>
+        </>
+      ) : (
+        <p>
+          Please login to add credentials <Link to="/login">login</Link> or{" "}
+          <Link to="/signup">signup</Link>
+        </p>
+      )}
+    </div>
+  );
+};
 
 export default CreatePass;
