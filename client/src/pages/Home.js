8f521cd46fa9155ca037ae241f500fae856468fe
Dani-Gore13 <daningore11@gmail.com>
2023-08-03 21:48:32 -0400

    password cards, again

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,12 +1,10 @@
-import React, { useState } from "react";
+import React from "react";
 import { useQuery, useMutation } from "@apollo/client";
 import { REMOVE_PASSWORD } from "../utils/mutations";
 import { QUERY_ME } from "../utils/queries";
 import { Link } from "react-router-dom";
 
-
 import Auth from "../utils/auth";
-
 
 const SavedPasswords = ({ passwords, isLoggedInUser = false }) => {
   const { loading, data } = useQuery(QUERY_ME);
@@ -42,49 +40,54 @@ const SavedPasswords = ({ passwords, isLoggedInUser = false }) => {
   if (loading) {
     return <h1> One Moment Please...</h1>;
   }
+
   return (
-    <div>
+    <div className="saved-passwords-container">
       {Auth.loggedIn() ? (
         <>
-
-      <div>
-        <h1> Your Credentials !</h1>
-      </div>
-      <div>
-        <h2 className="pt-5">
-          {userData.savedPassword?.length
-            ? `Viewing ${userData.savedPassword.length} saved ${
-                userData.savedPassword.length === 1 ? "password" : "passwords"
-              }:`
-            : "You have not saved any credentials yet!"}
-        </h2>
-
-        <div>
-          {userData.savedPasswords?.map((password) => {
-            return (
-              <div>
-                <main>
-                  <h3>This is your login information for {password.loginTo}</h3>
-                  <h4>Your Username: {password.savedUsername}</h4>
-                  <h4>Your Password: {password.savedPassword}</h4>
-                  <Link className="custom-button" to={`/update/${password.loginTo}`}>
-                Update Credentials
-              </Link>
-                  <button
-                    className="btn-block btn-danger"
-                    onClick={() => handleDeletePassword(password.loginTo)}
-                  >
-                    Delete Credentials for {password.loginTo} !
-                  </button>
-                </main>
-              </div>
-            );
-          })}
-        </div>
-      </div>
-      </>
-       ) : (
-
+          <div className="heading">
+            <h1>Your Credentials !</h1>
+          </div>
+          <div className="password-list">
+            <h2 className="sub-heading">
+              {userData.savedPassword?.length
+                ? `Viewing ${userData.savedPassword.length} saved ${
+                    userData.savedPassword.length === 1
+                      ? "password"
+                      : "passwords"
+                  }`
+                : "You have not saved any credentials yet!"}
+            </h2>
+            <div className="password-card-list">
+              {userData.savedPasswords?.map((password) => (
+                <div className="password-card" key={password.loginTo}>
+                  <main>
+                    <div className="password-card-content">
+                      <h3>
+                        This is your login information for {password.loginTo}
+                      </h3>
+                      <h4>Your Username: {password.savedUsername}</h4>
+                      <h4>Your Password: {password.savedPassword}</h4>
+                      <Link
+                        className="custom-button"
+                        to={`/update/${password.loginTo}`}
+                      >
+                        Update Credentials
+                      </Link>
+                      <button
+                        className="btn-block btn-danger"
+                        onClick={() => handleDeletePassword(password.loginTo)}
+                      >
+                        Delete Credentials for {password.loginTo} !
+                      </button>
+                    </div>
+                  </main>
+                </div>
+              ))}
+            </div>
+          </div>
+        </>
+      ) : (
         <p>
           Please login to view your credentials <Link to="/login">login</Link>{" "}
           or <Link to="/signup">signup</Link>
